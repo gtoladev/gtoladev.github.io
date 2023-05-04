eab713742407b1e9b7d84ceb01159a71d2f84658
ketident <31627461+ketident@users.noreply.github.com>
2023-05-04 09:21:02 +0200

    Create install.js

---
+++ netlify/functions/install.js
@@ -0,0 +1,9 @@
+exports.handler = async function (event, context) {
+  console.log({event}, {context});
+  const eventBody = JSON.parse(event.body)
+  console.log(event.body);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Installed" }),
+  };
+}
