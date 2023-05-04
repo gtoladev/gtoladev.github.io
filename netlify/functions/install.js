9f61a89a9c322181537c683f7f966165caa53402
ketident <31627461+ketident@users.noreply.github.com>
2023-05-04 09:24:07 +0200

    Update install.js

--- netlify/functions/install.js
+++ netlify/functions/install.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
   console.log({event}, {context});
-  const eventBody = JSON.parse(event.body)
+  //const eventBody = JSON.parse(event.body)
   console.log(event.body);
   return {
     statusCode: 200,
