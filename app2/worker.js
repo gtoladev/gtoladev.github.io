20fb94fa5e43f2dfebf39a82e4bad9a0d1c9059f
ketilezeti <gentitola14@hotmail.com>
2023-05-06 21:48:33 +0200

    f

---
+++ app2/worker.js
@@ -0,0 +1,14 @@
+(function () {
+  AP.request({
+      url: '/rest/api/3/search?fields=key,project,status&expand=transitions,changelog&startAt=0&maxResults=100000',
+      type: 'GET',
+      contentType: 'application/json',
+      headers: { Accept: 'application/json' },
+      success: function(responseText){
+        console.log("success from worker:"+responseText);
+      },
+      error: function(xhr, statusText, errorThrown){
+        console.log("err from worker: "+errorThrown);
+      }
+    });
+});
