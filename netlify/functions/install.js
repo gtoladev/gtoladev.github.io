9672ad0b54a5c214e8b4b5e725d9d4cd68ead350
ketident <31627461+ketident@users.noreply.github.com>
2023-05-11 12:08:02 +0200

    Update install.js

--- netlify/functions/install.js
+++ netlify/functions/install.js
@@ -57,20 +57,22 @@ exports.handler = async function (event, context) {
   const now = moment().utc();
   //GTODO
   //const req2 = jwt.fromMethodAndUrl('GET', '/rest/atlassian-connect/1/app/module/dynamic');
-  const req2 = jwt.fromMethodAndUrl('POST', '/rest/atlassian-connect/1/app/module/dynamic');
+  //const req2 = jwt.fromMethodAndUrl('POST', '/rest/atlassian-connect/1/app/module/dynamic');
   //console.log(req2);
   //const req2 = "GET&/rest/atlassian-connect/1/app/module/dynamic&";
+  const req3 = jwt.fromMethodAndUrl('POST', '/rest/api/2/search');
   const tokenData = {
       "iss": bodyJSON.key, //bodyJSON.clientKey,
       "iat": now.unix(),                    // The time the token is generated
       "exp": now.add(3, 'minutes').unix(),  // Token expiry time (recommend 3 minutes after issuing)
-      "qsh": jwt.createQueryStringHash(req2) // [Query String Hash](https://developer.atlassian.com/cloud/jira/platform/understanding-jwt/#a-name-qsh-a-creating-a-query-string-hash)
+      "qsh": jwt.createQueryStringHash(req3) // [Query String Hash](https://developer.atlassian.com/cloud/jira/platform/understanding-jwt/#a-name-qsh-a-creating-a-query-string-hash)
   };
 
   const token = jwt.encodeSymmetric(tokenData, bodyJSON.sharedSecret);
   console.log('GT token:'+token);
 
-  await fetch(bodyJSON.baseUrl+'/rest/atlassian-connect/1/app/module/dynamic', {
+  bodyData = {};
+  await fetch(bodyJSON.baseUrl+'/rest/api/2/search', {
     method: 'POST',
     body: JSON.stringify(bodyData),
     headers: {
@@ -85,6 +87,22 @@ exports.handler = async function (event, context) {
   })
   .then(text => console.log('GT Response txt: '+text))
   .catch(err => console.error('GT Response err: '+err));
+
+  // await fetch(bodyJSON.baseUrl+'/rest/atlassian-connect/1/app/module/dynamic', {
+    // method: 'POST',
+    // body: JSON.stringify(bodyData),
+    // headers: {
+      // 'Content-Type': 'application/json',
+      // 'Accept': 'application/json',
+      // 'Authorization': 'JWT '+token
+    // }
+  // })
+  // .then(response => {
+    // console.log('GT Response status: '+response.status+' '+response.statusText);
+    // return response.text();
+  // })
+  // .then(text => console.log('GT Response txt: '+text))
+  // .catch(err => console.error('GT Response err: '+err));
 
   //await fetch(bodyJSON.baseUrl+'/rest/atlassian-connect/1/app/module/dynamic', {
   //  method: 'GET',
